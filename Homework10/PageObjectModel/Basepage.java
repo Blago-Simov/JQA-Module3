2346d6e1e67f5fc9286939bee2202bd10329a480
Blagovest Simov <77917533+Blago-Simov@users.noreply.github.com>
2023-08-21 18:48:05 +0300

    Create Basepage.java

---
+++ Homework10/PageObjectModel/Basepage.java
@@ -0,0 +1,37 @@
+package Homework10PoM;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class BasePage {
+
+    static  WebDriver driver;
+
+    By elementsListTextBoxPanel = By.xpath("//*[@class='element-group'][1]/div/ul/li[1]");
+    By elementsListRadioButtonPanel = By.xpath("//*[@class='element-group'][1]/div/ul/li[3]");
+    By elementsListButtonsPanel = By.xpath("//*[@class='element-group'][1]/div/ul/li[5]");
+
+    public BasePage(WebDriver driver){
+        this.driver = driver;
+    }
+
+
+
+    public TextBoxPage loadTextBoxpage(){
+       driver.findElement(elementsListTextBoxPanel).click();
+        Assert.assertEquals("https://demoqa.com/text-box",driver.getCurrentUrl());
+        return new TextBoxPage(driver);
+    }
+    public RadioButtonPage loadRadioButtonPage(){
+        driver.findElement(elementsListRadioButtonPanel).click();
+        Assert.assertEquals("https://demoqa.com/radio-button",driver.getCurrentUrl());
+        return new RadioButtonPage(driver);
+    }
+    public ButtonsPage loadButtonsPage(){
+        driver.findElement(elementsListButtonsPanel).click();
+        Assert.assertEquals("https://demoqa.com/buttons",driver.getCurrentUrl());
+        return new ButtonsPage(driver);
+    }
+
+}
