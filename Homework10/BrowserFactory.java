ccaf6158af894cd7d934be40a39218d1799cd8e0
Blagovest Simov <77917533+Blago-Simov@users.noreply.github.com>
2023-08-25 22:42:33 +0300

    Update BrowserFactory.java

--- Homework10/BrowserFactory.java
+++ Homework10/BrowserFactory.java
@@ -1,35 +1,34 @@
 package Helpers;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserFactory {
-     WebDriver driver;
- public BrowserFactory(WebDriver driver){
 
-     this.driver = driver;
-  }
-    public static  WebDriver getBrowser(String browserName){
+    public static WebDriver driver;
 
-       WebDriver driver = null;
+    public static WebDriver getBrowser(String browserName) {
 
-       switch(browserName){
-            case "Chrome":
-                System.setProperty("webdriver.chrome.driver", "E:\\installers\\drivers\\Chromedriver\\chromedriver.exe");
-                return driver =  new ChromeDriver();
 
-            case "Firefox":
-                System.setProperty("webdriver.gecko.driver", "E:\\installers\\drivers\\geckodriver-v0.27.0-win64\\geckodriver.exe");
-                return driver = new FirefoxDriver();
+            switch (browserName) {
+                case "Chrome":
+                    System.setProperty("webdriver.chrome.driver", "E:\\installers\\drivers\\Chromedriver\\chromedriver.exe");
+                    return driver = new ChromeDriver();
 
-            case "Edge":
-                System.setProperty("webdriver.edge.driver", "E:\\installers\\drivers\\Edgedriver\\msedgedriver.exe");
-                return   driver = new EdgeDriver();
-            default:
-                throw  new IllegalArgumentException("Undefined or unsupported browser type" + browserName);
+                case "Firefox":
+                    System.setProperty("webdriver.gecko.driver", "E:\\installers\\drivers\\geckodriver-v0.27.0-win64\\geckodriver.exe");
+                    return driver = new FirefoxDriver();
+
+                case "Edge":
+                    System.setProperty("webdriver.edge.driver", "E:\\installers\\drivers\\Edgedriver\\msedgedriver.exe");
+                    return driver = new EdgeDriver();
+                default:
+                    throw new IllegalArgumentException("Undefined or unsupported browser type" + browserName);
+            }
+
+
         }
 
     }
-
-}
