f794e60ee7879546a6bfc86edfa2a13fbd18f4fd
Blagovest Simov <77917533+Blago-Simov@users.noreply.github.com>
2023-08-25 22:47:09 +0300

    Update RadioButtonFactoryPage.java

--- Homework10/PageFactory/RadioButtonFactoryPage.java
+++ Homework10/PageFactory/RadioButtonFactoryPage.java
@@ -11,39 +11,40 @@ import org.openqa.selenium.support.PageFactory;
 public class RadioButtonFactoryPage {
     WebDriver driver;
 
-    @FindBy(how = How.CSS,using = "#yesRadio" )
+    @FindBy(how = How.CSS, using = "#yesRadio")
     WebElement yesRadioButton;
-    @FindBy(how = How.ID,using = "impressiveRadio" )
+    @FindBy(how = How.ID, using = "impressiveRadio")
     WebElement impressiveRadioButton;
-    @FindBy(how = How.ID,using = "noRadio")
-    WebElement noRadioButton ;
-    @FindBy(how = How.XPATH,using = "//*[@class='mt-3']")
-    WebElement resultText ;
+    @FindBy(how = How.ID, using = "noRadio")
+    WebElement noRadioButton;
+    @FindBy(how = How.XPATH, using = "//*[@class='mt-3']")
+    WebElement resultText;
 
-    public RadioButtonFactoryPage(WebDriver driver){
+    public RadioButtonFactoryPage(WebDriver driver) {
 
         this.driver = driver;
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
 
-    public void clickOnYesButton(){
+    public void clickOnYesButton() {
 
-        JavascriptExecutor js = (JavascriptExecutor)driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();", yesRadioButton);
 
     }
 
-    public void clickOnImpressiveButton(){
+    public void clickOnImpressiveButton() {
 
-        JavascriptExecutor js = (JavascriptExecutor)driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();", impressiveRadioButton);
     }
 
-    public boolean IsNoButtonEnabled(){
+    public boolean IsNoButtonEnabled() {
         return (noRadioButton).isEnabled();
     }
-    public String resultTextField(){
-       return  (resultText).getText();
+
+    public String resultTextField() {
+        return (resultText).getText();
 
     }
 }
