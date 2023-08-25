73066cfee76901840db07d97ec203d857c7ae511
Blagovest Simov <77917533+Blago-Simov@users.noreply.github.com>
2023-08-25 23:07:08 +0300

    Create SetUpAndTearDown.java

---
+++ Homework10/PageObjectModel/SetUpAndTearDown.java
@@ -0,0 +1,35 @@
+package Homework10PoM;
+
+import Helpers.BrowserFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SetUpAndTearDown {
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = BrowserFactory.getBrowser("Firefox");
+
+        if (driver != null) {
+            driver.get("https://demoqa.com/elements");
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+            driver.manage().window().maximize();
+
+        }
+
+
+    }
+
+    @After
+    public void tearDown() {
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
