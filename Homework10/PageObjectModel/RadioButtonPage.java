6348c5990724fc15b7ce969d3e0ce2a4fbfaefc4
Blagovest Simov <77917533+Blago-Simov@users.noreply.github.com>
2023-08-25 23:05:04 +0300

    Update RadioButtonPage.java

--- Homework10/PageObjectModel/RadioButtonPage.java
+++ Homework10/PageObjectModel/RadioButtonPage.java
@@ -7,35 +7,36 @@ import org.openqa.selenium.WebElement;
 
 public class RadioButtonPage {
 
-    WebDriver driver;
+     WebDriver driver;
 
     By yesRadioButton = By.cssSelector("#yesRadio");
     By impressiveRadioButton = By.id("impressiveRadio");
     By noRadioButton = By.id("noRadio");
     By resultText = By.xpath("//*[@class='mt-3']");
 
-    public RadioButtonPage(WebDriver driver){
+    public RadioButtonPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void clickOnYesButton(){
+    public void clickOnYesButton() {
         WebElement yesButton = driver.findElement(yesRadioButton);
-        JavascriptExecutor js = (JavascriptExecutor)driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();", yesButton);
 
     }
 
-    public void clickOnImpressiveButton(){
+    public void clickOnImpressiveButton() {
         WebElement impressiveButton = driver.findElement(impressiveRadioButton);
-        JavascriptExecutor js = (JavascriptExecutor)driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();", impressiveButton);
     }
 
-    public boolean IsNoButtonEnabled(){
+    public boolean IsNoButtonEnabled() {
         return driver.findElement(noRadioButton).isEnabled();
     }
-    public String resultTextField(){
-       return   driver.findElement(resultText).getText();
+
+    public String resultTextField() {
+        return driver.findElement(resultText).getText();
 
     }
 }
