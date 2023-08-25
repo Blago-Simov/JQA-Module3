fcb5e306ad6779ee93498c957d12f4418e4411ed
Blagovest Simov <77917533+Blago-Simov@users.noreply.github.com>
2023-08-25 22:45:42 +0300

    Update ButtonsFactoryPage.java

--- Homework10/PageFactory/ButtonsFactoryPage.java
+++ Homework10/PageFactory/ButtonsFactoryPage.java
@@ -12,47 +12,53 @@ public class ButtonsFactoryPage {
 
     WebDriver driver;
 
-    @FindBy(how = How.ID,using="doubleClickBtn")
+    @FindBy(how = How.ID, using = "doubleClickBtn")
     WebElement doubleClickButton;
-    @FindBy(how = How.ID,using = "rightClickBtn")
-    WebElement rightClickButton ;
-    @FindBy(how = How.XPATH,using = "/html[1]//div[3]/button[1]")
+    @FindBy(how = How.ID, using = "rightClickBtn")
+    WebElement rightClickButton;
+    @FindBy(how = How.XPATH, using = "/html[1]//div[3]/button[1]")
     WebElement dynamicClickButton;
-    @FindBy(how = How.CSS,using = "div > p#doubleClickMessage")
-    WebElement doubleClickMessage ;
-    @FindBy(how = How.CSS,using = "div > p#rightClickMessage" )
-    WebElement rightClickMessage ;
-    @FindBy(how = How.CSS,using = "div > p#dynamicClickMessage" )
+    @FindBy(how = How.CSS, using = "div > p#doubleClickMessage")
+    WebElement doubleClickMessage;
+    @FindBy(how = How.CSS, using = "div > p#rightClickMessage")
+    WebElement rightClickMessage;
+    @FindBy(how = How.CSS, using = "div > p#dynamicClickMessage")
     WebElement dynamicClickMessage;
 
-    public ButtonsFactoryPage(WebDriver driver){
+    public ButtonsFactoryPage(WebDriver driver) {
 
         this.driver = driver;
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
 
-    public void doubleClick(){
-        Actions act = new Actions(driver);
+    Actions act;
+
+    public void doubleClick() {
+        act = new Actions(driver);
         act.doubleClick((doubleClickButton)).perform();
     }
-    public void rightClick(){
-        Actions act = new Actions(driver);
+
+    public void rightClick() {
+        act = new Actions(driver);
         act.contextClick((rightClickButton)).perform();
     }
-    public void dynamicClick(){
+
+    public void dynamicClick() {
         dynamicClickButton.click();
 
     }
 
-    public String doubleClickMessage(){
+    public String doubleClickMessage() {
         return (doubleClickMessage).getText();
 
     }
-    public String rightClickMessage(){
+
+    public String rightClickMessage() {
         return (rightClickMessage).getText();
 
     }
-    public String dynamicClickMessage(){
-       return  (dynamicClickMessage).getText();
+
+    public String dynamicClickMessage() {
+        return (dynamicClickMessage).getText();
     }
 }
