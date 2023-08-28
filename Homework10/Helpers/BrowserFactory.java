05777c2a1789da659c71b4be7498cc07f0173c98
Blagovest Simov <77917533+Blago-Simov@users.noreply.github.com>
2023-08-28 21:04:27 +0300

    Create BrowserFactory.java

---
+++ Homework10/Helpers/BrowserFactory.java
@@ -0,0 +1,34 @@
+package Helpers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserFactory {
+
+    public static WebDriver driver;
+
+    public static WebDriver getBrowser(String browserName) {
+
+
+            switch (browserName) {
+                case "Chrome":
+                    System.setProperty("webdriver.chrome.driver", "E:\\installers\\drivers\\Chromedriver\\chromedriver.exe");
+                    return driver = new ChromeDriver();
+
+                case "Firefox":
+                    System.setProperty("webdriver.gecko.driver", "E:\\installers\\drivers\\geckodriver-v0.27.0-win64\\geckodriver.exe");
+                    return driver = new FirefoxDriver();
+
+                case "Edge":
+                    System.setProperty("webdriver.edge.driver", "E:\\installers\\drivers\\Edgedriver\\msedgedriver.exe");
+                    return driver = new EdgeDriver();
+                default:
+                    throw new IllegalArgumentException("Undefined or unsupported browser type" + browserName);
+            }
+
+
+        }
+
+    }
