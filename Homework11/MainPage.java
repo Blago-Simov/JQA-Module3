e097763d0c6872897ae6e86054eaba98c1cf2962
Blagovest Simov <77917533+Blago-Simov@users.noreply.github.com>
2023-09-03 23:01:34 +0300

    Update MainPage.java

--- Homework11/MainPage.java
+++ Homework11/MainPage.java
@@ -1,18 +1,15 @@
 package Homework11;
 
-import Helpers.WaitHelper;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainPage {
     public static WebDriver driver;
 
-    public WebDriverWait wait;
-   //WaitHelper Class from Homework10 Helpers
-    WaitHelper waitHelper;
     By elementsListButtonsPanel = By.xpath("//*[@class='element-group'][1]/div/ul/li[5]");
 
     public MainPage(WebDriver driver) {
@@ -21,7 +18,17 @@ public class MainPage {
 
 
     public ButtonsPageWithCustomExpectedConditions loadButtonsPage() {
-        waitHelper.customWait((WebElement) elementsListButtonsPanel,20);
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until((ExpectedCondition<Boolean>) driver -> {
+            WebElement element = driver.findElement(elementsListButtonsPanel);
+            if(element!=null){
+                if(element.isEnabled()&& element.isDisplayed())
+                    return true;
+                else
+                    return false;
+            }
+            return false;
+        });
         driver.findElement(elementsListButtonsPanel).click();
         Assert.assertEquals("https://demoqa.com/buttons", driver.getCurrentUrl());
         return new ButtonsPageWithCustomExpectedConditions(driver);
